23f7b0c60bce5e735a1182a9f68bc6181ed02d47
KevinApp2023 <goodmaxcolombia@gmail.com>
2024-12-15 14:15:58 -0500

    master

---
+++ dist/cms.php
@@ -0,0 +1,26 @@
+<?php include("../config/conex.php"); ?>
+<?php include("../config/config.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <?php include("../dist/assets/mod/head.php");?>
+  <body>
+    <div class="container-scroller">
+    <?php include("../dist/assets/mod/navbar.php");?>
+  
+        <div class="main-panel">
+          <div class="content-wrapper" >
+          
+           <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+          
+
+          </div>
+    <?php  include("../dist/assets/mod/footer.php"); ?>
+
+          
+        </div>
+      </div>
+    </div>
+    
+    <?php  include("../dist/assets/mod/mod_js.php"); ?>
+  </body>
+</html>
