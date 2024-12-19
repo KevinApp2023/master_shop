3522c6abf0d3efb1909f0af98631b049adf31524
KevinApp2023 <goodmaxcolombia@gmail.com>
2024-12-19 16:37:49 -0500

    master

--- dist/assets/etc/subir_galeria_producto.php
+++ dist/assets/etc/subir_galeria_producto.php
@@ -26,13 +26,29 @@ if ($stmt = $conex->prepare($consult_img_productos)) {
             }
         }
     } else {
-        $sql = "UPDATE products SET ref = ? WHERE id = ?";
-        if ($stmt = $conex->prepare($sql)) {
-            $ref = "Valor_de_ref";  
-            $stmt->bind_param("si", $ref, $id);
-            $stmt->execute();
-            $stmt->close();
+
+
+        if (isset($_FILES['agregar_nueva_galeria_data']) && $_FILES['agregar_nueva_galeria_data']['error'] === 0) {
+            $agregar_nueva_galeria_data = $_FILES['agregar_nueva_galeria_data'];
+            $agregar_nueva_galeria_data_path = '/img/products/' . date('Y_m_d_H_i_s_') . basename($agregar_nueva_galeria_data['name']);
+            $agregar_nueva_galeria_data_path_s = '../../../img/products/' . date('Y_m_d_H_i_s_')  . basename($agregar_nueva_galeria_data['name']);
+
+            if (move_uploaded_file($agregar_nueva_galeria_data['tmp_name'], $agregar_nueva_galeria_data_path_s)) {
+                $sql = "UPDATE products SET img_producto = ? WHERE id = ?";
+                if ($stmt = $conex->prepare($sql)) {
+                    $stmt->bind_param("si", $agregar_nueva_galeria_data_path, $id);
+                    $stmt->execute();
+                    $stmt->close();
+                }
+                $sql_g = "INSERT INTO img_productos (img, productos) VALUES ('$agregar_nueva_galeria_data_path', '$id')";
+                if ($conex->query($sql_g) === TRUE) {
+                }
+            }
         }
+
+
+
+        
     }
 }
 ?>
